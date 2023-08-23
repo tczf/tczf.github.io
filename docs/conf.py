5ee57239ec7424f221ad5dbd614f9d16a8aa6436
tczf <1509500509@qq.com>
2023-08-23 14:10:03 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+#extensions = ['myst_parser']
 source_suffix=['.rst','.md']
 
 templates_path = ['_templates']
